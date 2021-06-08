190b17f5bb3de4364b5c1ae011589799e99ce837
Archit Patel <architps07@gmail.com>
2021-06-08 18:55:20 -0400

    Error Handling

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,16 @@
+namespace API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message = null, string deatils = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Deatils = deatils;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string  Deatils { get; set; }
+    }
+}
